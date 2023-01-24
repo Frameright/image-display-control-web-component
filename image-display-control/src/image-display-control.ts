04efbd76d33926a0bb4f976dada71dee30a6b3fd
Aurelien Lourot <software@lourot.dev>
2023-01-24 15:01:17 +0100

    Add Safari support

--- image-display-control/src/image-display-control.ts
+++ image-display-control/src/image-display-control.ts
@@ -1,3 +1,6 @@
+// Polyfill for Safari, see https://github.com/ungap/custom-elements
+import '@ungap/custom-elements';
+
 import { ImageDisplayControl } from './ImageDisplayControl.js';
 
 window.customElements.define('image-display-control', ImageDisplayControl, {
